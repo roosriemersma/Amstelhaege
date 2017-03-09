92c5dab6a6b022714bf6bbc78041f6b38f9091a2
MaartjeMolenaar <maartjemolenaar1997@gmail.com>
2017-03-09 17:56:26 +0100

    Regels 2.0

--- Amstelhaege_probleem.py
+++ Amstelhaege_probleem.py
@@ -1,8 +1,8 @@
 print("Maartje is de shit")
 #160 X 180 meter = 28800 m2
 #20% water = 5760 m2
-#60% 1sgezins  8x8      2 meter   285000eu   3%
-#25% bungalo   10x7.5   3 meter   399000eu   4%
-#15% maison    11x10.5  6 meter   610000eu   6%
+#60% 1sgezins  8x8 = 16m2            2 meter   285000eu   3%
+#25% bungalo   10x7.5 = 75m2         3 meter   399000eu   4%
+#15% maison    11x10.5 = 115.5m2     6 meter   610000eu   6%
 
 

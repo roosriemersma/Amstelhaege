47048038bd8daadfa2516d6ffe55c5c6518ae415
RoosRiemersma <roos.riemersma@hotmail.com>
2017-03-16 17:35:19 +0100

    Art

--- Amstelhaege_probleem.py
+++ Amstelhaege_probleem.py
@@ -16,3 +16,17 @@ maison = {11, 10.5, 610000, 0.06}
 import tkinter
 top = tkinter.Tk()
 top.mainloop()
+
+from tkinter import *
+
+master = Tk()
+
+w = Canvas(master, width=200, height=100)
+w.pack()
+
+w.create_line(0, 0, 200, 100)
+w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+
+w.create_rectangle(50, 25, 150, 75, fill="blue")
+
+mainloop()

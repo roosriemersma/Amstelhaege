689be5dda9d3cba8030bd34d0f7d133a160e5504
borditewig <borisquinten@hotmail.com>
2017-03-23 18:06:45 +0100

    belangrijke bron toegevoegd

--- Amstelhaege_probleem.py
+++ Amstelhaege_probleem.py
@@ -37,20 +37,34 @@ width = 160
 height = 180
 hoeveelHuizen = [20, 40, 60]
 maxHuizen = random.choice(hoeveelHuizen)
+huizenCoordinaten = []
+
+def vindCoordinaten(breedte, diepte):
+    coordinatenInvalid = TRUE
+    while coordinatenInvalid:
+        randomX = randint(0, int(160 - breedte))
+        randomY = randint(0, int(180 - diepte))
+        for i in range len(huizenCoordinaten)
+        if randomX >= 0 and randomX <= 10 and randomY >= 0 and randomY <= 10
+            coordinatenInvalid = FALSE
 
 def huizenPlaatsen():
     for j in range(int(eengezinswoning.percentage * maxHuizen)):
-        randomX = randint(0, int(160 - eengezinswoning.breedte))
-        randomY = randint(0, int(180 - eengezinswoning.diepte))
+            randomX = randint(0, int(160 - eengezinswoning.breedte))
+            randomY = randint(0, int(180 - eengezinswoning.diepte))
         map.create_rectangle(randomX, randomY, randomX + eengezinswoning.breedte, randomY + eengezinswoning.diepte, fill="red")
+        huizenCoordinaten.append(randomX, randomY)
     for k in range(int(bungalo.percentage * maxHuizen)):
         randomX = randint(0, int(160 - bungalo.breedte))
         randomY = randint(0, int(180 - bungalo.diepte))
         map.create_rectangle(randomX, randomY, randomX + bungalo.breedte, randomY + bungalo.diepte, fill="blue")
+        huizenCoordinaten.append(randomX, randomY)
     for l in range(int(maison.percentage * maxHuizen)):
         randomX = randint(0, int(160 - maison.breedte))
         randomY = randint(0, int(180 - maison.diepte))
         map.create_rectangle(randomX, randomY, randomX + maison.breedte, randomY + maison.diepte, fill="yellow")
+        huizenCoordinaten.append(randomX, randomY)
+
 
 #def huizenPlaatsen2()
 #    for i in range(int())

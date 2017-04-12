c8b8ce6e2f3021a1339ebbe37fde513ccdb59829
RoosRiemersma <roos.riemersma@hotmail.com>
2017-04-12 16:49:39 +0200

    Afstand tot andere woning

--- Amstelhaege_probleem.py
+++ Amstelhaege_probleem.py
@@ -39,6 +39,45 @@ def vindCoordinaten(typeWoning):
                 coordinatenValid = False
     return nieuwCoordinaat
 
+def vrijstandTussen(woningA, woningB):
+    x1 = woningA.x
+    y1 = woningA.y
+    x1b = x1 + woningA.breedte
+    y1b = y1 + woningA.diepte
+    x2 = woningB.x
+    y2 = woningB.y
+    x2b = x2 + woningB.breedte
+    y2b = y2 + woningB.diepte
+
+    left = x2b < x1
+    right = x1b < x2
+    bottom = y2b < y2
+    top = y1b < y2
+    if top and left:
+        print("afstand dist((x1, y1b), (x2b, y2)) = ", dist((x1, y1b), (x2b, y2)))    #hier moet Euclidean distance berekend worden
+        return dist((x1, y1b), (x2b, y2))
+    elif left and bottom:
+        print("afstand dist((x1, y1), (x2b, y2b)) = ", dist((x1, y1), (x2b, y2b)))   #hier moet Euclidean distance berekend worden
+        return dist((x1, y1), (x2b, y2b))
+    elif bottom and right:
+        print("afstand dist((x1b, y1), (x2, y2b)) = ", dist((x1b, y1), (x2, y2b)))   #hier moet Euclidean distance berekend worden
+        return dist((x1b, y1), (x2, y2b))
+    elif right and top:
+        print("afstand dist((x1b, y1b), (x2, y2)) = ", dist((x1b, y1b), (x2, y2)))    #hier moet Euclidean distance berekend worden
+        return dist((x1b, y1b), (x2, y2))
+    elif left:
+        print("afstand x1 - x2b = ", x1 - x2b)
+        return x1 - x2b
+    elif right:
+        print("afstand x2 - x1b = ", x2 - x1b)
+        return x2 - x1b
+    elif bottom:
+        print("afstand y1 - y2b = ", y1 - y2b)
+        return y1 - y2b
+    elif top:
+        print("afstand y2 - y1b = ", y2 - y1b)
+        return y2 - y1b
+
 def plaatsWoning(typeWoning):
     nieuwCoordinaat = vindCoordinaten(typeWoning)
     x = nieuwCoordinaat[0]
@@ -60,12 +99,7 @@ for j in range(int(Woning.Bungalo.aandeelHuizen * maxHuizen)):
 for k in range(int(Woning.Maison.aandeelHuizen * maxHuizen)):
     plaatsWoning(Woning.Maison)
 
-
-def tekenWoning(woningen):
-    for woning in woningen:
-        map.create_rectangle(woning.x, woning.y , woning.x + woning.breedte, woning.y + woning.diepte, fill = woning.kleur)
-
-
+vrijstandTussen(woningen[3], woningen[5])
 
 #visualiseren
 master = Tk()
